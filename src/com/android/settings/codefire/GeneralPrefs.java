58ea3b08c63d1f12d8593bfc54837c7505d165e5
Synergy Dev <synergycomputing@gmail.com>
2012-09-09 05:31:30 +0000

    cfX: major cleanup/rework

--- src/com/android/settings/codefire/GeneralPrefs.java
+++ src/com/android/settings/codefire/GeneralPrefs.java
@@ -1,4 +1,5 @@
-package com.android.settings;
+
+package com.android.settings.codefire;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -38,10 +39,10 @@ import com.android.settings.colorpicker.ColorPickerPreference;
 
 import java.io.IOException;
 
-public class CodefireSettings extends SettingsFragment
+public class GeneralPrefs extends SettingsFragment
     implements Preference.OnPreferenceChangeListener {
 
-    private final static String TAG = CodefireSettings.class.getSimpleName();
+    private final static String TAG = GeneralPrefs.class.getSimpleName();
 
     private static final String TRACKBALL_WAKE_TOGGLE = "pref_trackball_wake_toggle";
     private static final String TRACKBALL_UNLOCK_TOGGLE = "pref_trackball_unlock_toggle";
@@ -49,53 +50,26 @@ public class CodefireSettings extends SettingsFragment
     private static final String DISABLE_BOOTANIMATION_PERSIST_PROP = "persist.sys.nobootanimation";
     private static final String DISABLE_BOOTANIMATION_DEFAULT = "0";
     private static final String PREF_RECENT_KILL_ALL = "recent_kill_all";
-    private static final String KEY_DUAL_PANE = "dual_pane";
-    private static final String SHOW_BRIGHTNESS_TOGGLESLIDER = "pref_show_brightness_toggleslider";
     private static final String KILL_APP_LONGPRESS_BACK_TIMEOUT = "pref_kill_app_longpress_back_timeout";
-    private static final String KEY_NAVIGATION_BAR = "navigation_bar";
-    private static final String KEY_LCD_DENSITY = "lcd_density";
-    private static final String STATUS_BAR_CLOCK_STYLE = "status_bar_clock_style";
-    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
 
     private ContentResolver mCr;
     private PreferenceScreen mPrefSet;
-    private PreferenceScreen mNavigationBar;
 
     private CheckBoxPreference mTrackballWake;
     private CheckBoxPreference mTrackballUnlockScreen;
     private CheckBoxPreference mDisableBootanimPref;
     private CheckBoxPreference mRecentKillAll;
-    private CheckBoxPreference mDualPane;
-    private CheckBoxPreference mShowBrightnessToggleslider;
 
     private EditTextPreference mKillAppLongpressBackTimeout;
-
-    private Preference mCustomLabel;
-    String mCustomLabelText = null;
-
-    private ListPreference mStatusBarClockStyle;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        addPreferencesFromResource(R.xml.codefire_settings);
+        addPreferencesFromResource(R.xml.codefire_general);
 
         mPrefSet = getPreferenceScreen();
         mCr = getContentResolver();
-        mNavigationBar = (PreferenceScreen) findPreference(KEY_NAVIGATION_BAR);
-        mStatusBarClockStyle = (ListPreference) mPrefSet.findPreference(STATUS_BAR_CLOCK_STYLE);
-
-        /* Custom Carrier Label */
-        mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
-        updateCustomLabelTextSummary();
-
-        /* Clock Style */
-        int statusBarClockStyle = Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
-                Settings.System.STATUS_BAR_CLOCK_STYLE, 1);
-        mStatusBarClockStyle.setValue(String.valueOf(statusBarClockStyle));
-        mStatusBarClockStyle.setSummary(mStatusBarClockStyle.getEntry());
-        mStatusBarClockStyle.setOnPreferenceChangeListener(this);
 
         /* Trackball wake pref */
         mTrackballWake = (CheckBoxPreference) mPrefSet.findPreference(
@@ -117,25 +91,6 @@ public class CodefireSettings extends SettingsFragment
         mRecentKillAll.setChecked(Settings.System.getInt(getContentResolver(),
                 Settings.System.RECENT_KILL_ALL_BUTTON, 0) == 1);
         mRecentKillAll.setOnPreferenceChangeListener(this);
-
-        /* Dual pane toggle */
-        mDualPane = (CheckBoxPreference) mPrefSet.findPreference(
-                KEY_DUAL_PANE);
-        mDualPane.setChecked(Settings.System.getInt(getContentResolver(),
-                Settings.System.DUAL_PANE_SETTINGS, 0) == 1);
-        mDualPane.setOnPreferenceChangeListener(this);
-
-        /* Notification Area Brightness Toggleslider pref */
-        mShowBrightnessToggleslider = (CheckBoxPreference) mPrefSet.findPreference(
-                SHOW_BRIGHTNESS_TOGGLESLIDER);
-        mShowBrightnessToggleslider.setChecked(Settings.System.getInt(getContentResolver(),
-                Settings.System.SHOW_BRIGHTNESS_TOGGLESLIDER, 0) == 1);
-        mShowBrightnessToggleslider.setOnPreferenceChangeListener(this);
-
-        /* Disable BootAnimation Toggle */
-        mDisableBootanimPref = (CheckBoxPreference) mPrefSet.findPreference(DISABLE_BOOTANIMATION_PREF);
-        String disableBootanim = SystemProperties.get(DISABLE_BOOTANIMATION_PERSIST_PROP, DISABLE_BOOTANIMATION_DEFAULT);
-        mDisableBootanimPref.setChecked("1".equals(disableBootanim));
 
         /* Kill App Longpress Back timeout duration pref */
         mKillAppLongpressBackTimeout = (EditTextPreference) mPrefSet.findPreference(KILL_APP_LONGPRESS_BACK_TIMEOUT);
@@ -176,46 +131,13 @@ public class CodefireSettings extends SettingsFragment
         if (preference == mDisableBootanimPref) {
             SystemProperties.set(DISABLE_BOOTANIMATION_PERSIST_PROP,
                     mDisableBootanimPref.isChecked() ? "1" : "0");
-        } else if (preference == mCustomLabel) {
-            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
-
-            alert.setTitle(R.string.custom_carrier_label_title);
-            alert.setMessage(R.string.custom_carrier_label_explain);
-
-            // Set an EditText view to get user input
-            final EditText input = new EditText(getActivity());
-            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
-            alert.setView(input);
-
-            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    String value = ((Spannable) input.getText()).toString();
-                    Settings.System.putString(getActivity().getContentResolver(),
-                            Settings.System.CUSTOM_CARRIER_LABEL, value);
-                    updateCustomLabelTextSummary();
-                    Intent i = new Intent();
-                    i.setAction("com.android.settings.LABEL_CHANGED");
-                    getActivity().getApplicationContext().sendBroadcast(i);
-                }
-            });
-            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
-                public void onClick(DialogInterface dialog, int whichButton) {
-                    // Canceled.
-                }
-            });
-
-            alert.show();
-        } else {
-            // If we didn't handle it, let preferences handle it.
-            return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
-
         return true;
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         String key = preference.getKey();
-        
+
         if (KILL_APP_LONGPRESS_BACK_TIMEOUT.equals(key)) {
             try {
                 int timeout = Integer.parseInt((String) newValue);
@@ -236,16 +158,6 @@ public class CodefireSettings extends SettingsFragment
             Settings.System.putInt(mCr, Settings.System.TRACKBALL_UNLOCK_SCREEN, (Boolean) newValue ? 1 : 0);
         } else if (PREF_RECENT_KILL_ALL.equals(key)) {
             Settings.System.putInt(mCr, Settings.System.RECENT_KILL_ALL_BUTTON, (Boolean) newValue ? 1 : 0);
-        } else if (KEY_DUAL_PANE.equals(key)) {
-            Settings.System.putInt(mCr, Settings.System.DUAL_PANE_SETTINGS, (Boolean) newValue ? 1 : 0);
-        } else if (SHOW_BRIGHTNESS_TOGGLESLIDER.equals(key)) {
-            Settings.System.putInt(mCr, Settings.System.SHOW_BRIGHTNESS_TOGGLESLIDER, (Boolean) newValue ? 1 : 0);
-        } else if (STATUS_BAR_CLOCK_STYLE.equals(key)) {
-            int statusBarClockStyle = Integer.valueOf((String) newValue);
-            int index = mStatusBarClockStyle.findIndexOfValue((String) newValue);
-            Settings.System.putInt(getActivity().getApplicationContext().getContentResolver(),
-                    Settings.System.STATUS_BAR_CLOCK_STYLE, statusBarClockStyle);
-            mStatusBarClockStyle.setSummary(mStatusBarClockStyle.getEntries()[index]);
         }
         return true;
     }
---
+++ src/com/android/settings/codefire/ThemePrefs.java
@@ -0,0 +1,141 @@
+
+package com.android.settings.codefire;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.ContentResolver;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.SystemProperties;
+import android.preference.CheckBoxPreference;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
+import android.provider.Settings;
+import android.text.Spannable;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+
+import com.android.settings.R;
+import com.android.settings.SettingsFragment;
+
+import com.android.settings.colorpicker.ColorPickerPreference;
+
+import java.io.IOException;
+
+public class ThemePrefs extends SettingsFragment
+    implements Preference.OnPreferenceChangeListener {
+
+    private final static String TAG = ThemePrefs.class.getSimpleName();
+
+    private static final String KEY_DUAL_PANE = "dual_pane";
+    private static final String KEY_NAVIGATION_BAR = "navigation_bar";
+    private static final String KEY_LCD_DENSITY = "lcd_density";
+    private static final String PREF_CUSTOM_CARRIER_LABEL = "custom_carrier_label";
+
+    private ContentResolver mCr;
+    private PreferenceScreen mPrefSet;
+    private PreferenceScreen mNavigationBar;
+    private CheckBoxPreference mDualPane;
+
+    private Preference mCustomLabel;
+    String mCustomLabelText = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        addPreferencesFromResource(R.xml.codefire_theme);
+
+        mPrefSet = getPreferenceScreen();
+        mCr = getContentResolver();
+        mNavigationBar = (PreferenceScreen) findPreference(KEY_NAVIGATION_BAR);
+
+        /* Custom Carrier Label */
+        mCustomLabel = findPreference(PREF_CUSTOM_CARRIER_LABEL);
+        updateCustomLabelTextSummary();
+
+        /* Dual pane toggle */
+        mDualPane = (CheckBoxPreference) mPrefSet.findPreference(
+                KEY_DUAL_PANE);
+        mDualPane.setChecked(Settings.System.getInt(getContentResolver(),
+                Settings.System.DUAL_PANE_SETTINGS, 0) == 1);
+        mDualPane.setOnPreferenceChangeListener(this);
+        }
+    }
+
+    private void updateCustomLabelTextSummary() {
+        mCustomLabelText = Settings.System.getString(getActivity().getContentResolver(),
+                Settings.System.CUSTOM_CARRIER_LABEL);
+        if (mCustomLabelText == null || mCustomLabelText.length() == 0) {
+            mCustomLabel.setSummary(R.string.custom_carrier_label_notset);
+        } else {
+            mCustomLabel.setSummary(mCustomLabelText);
+        }
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+        if (preference == mCustomLabel) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getActivity());
+
+            alert.setTitle(R.string.custom_carrier_label_title);
+            alert.setMessage(R.string.custom_carrier_label_explain);
+
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getActivity());
+            input.setText(mCustomLabelText != null ? mCustomLabelText : "");
+            alert.setView(input);
+
+            alert.setPositiveButton(getResources().getString(R.string.ok), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    String value = ((Spannable) input.getText()).toString();
+                    Settings.System.putString(getActivity().getContentResolver(),
+                            Settings.System.CUSTOM_CARRIER_LABEL, value);
+                    updateCustomLabelTextSummary();
+                    Intent i = new Intent();
+                    i.setAction("com.android.settings.LABEL_CHANGED");
+                    getActivity().getApplicationContext().sendBroadcast(i);
+                }
+            });
+            alert.setNegativeButton(getResources().getString(R.string.cancel), new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int whichButton) {
+                    // Canceled.
+                }
+            });
+
+            alert.show();
+        } else {
+            // If we didn't handle it, let preferences handle it.
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
+        }
+
+        return true;
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String key = preference.getKey();
+
+        if (KEY_DUAL_PANE.equals(key)) {
+            Settings.System.putInt(mCr, Settings.System.DUAL_PANE_SETTINGS, (Boolean) newValue ? 1 : 0);
+        }
+        return true;
+    }
+
+}
